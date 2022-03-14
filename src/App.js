a731d20c5861575f5bec7105d50789bd031c2d5f
Milica <magiflowerchild@gmial.com>
2022-03-14 23:17:22 +0100

    rendered animals table

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [zivotinje, setZivotinje] = useState([
+    {
+      species: "cat",
+      name: "Miki",
+      birthday: new Date(2021, 2, 3).toLocaleDateString(),
+    },
+    {
+      species: "parrot",
+      name: "Pero",
+      birthday: new Date(2015, 5, 7).toLocaleDateString(),
+    },
+    {
+      species: "dog",
+      name: "Rex",
+      birthday: new Date(2013, 5, 1).toLocaleDateString(),
+    },
+    {
+      species: "cat",
+      name: "Suki",
+      birthday: new Date(2015, 2, 10).toLocaleDateString(),
+    },
+    {
+      species: "parrot",
+      name: "Polly",
+      birthday: new Date(2010, 1, 2).toLocaleDateString(),
+    },
+  ]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <table>
+        <thead>
+          <tr>
+            <th>species</th>
+            <th>name</th>
+            <th>birthday</th>
+          </tr>
+        </thead>
+        <tbody>
+          {zivotinje.map((item, index) => (
+            <tr key={index}>
+              <td>{item.species}</td>
+              <td>{item.name}</td>
+              <td>{item.birthday}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
